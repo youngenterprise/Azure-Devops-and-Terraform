f4f0e17f835ebefa8d65d84d7dded5731f6c1fe1
Nakaw Young <nakawyoung@gmail.com>
2020-06-20 12:54:17 -0400

    added main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "azurerm"{
+    version="2.5.0"
+    features{}
+}
+resource "azurerm_resource_group" "tf_test"{
+    name="tfmainrg"
+    location="centralus"
+}
+resource "azurerm_container_group" "tfcg_test"{
+    name="weatherapi"
+    location=azurerm_resource_group.tf_test.location
+    resource_group_name=azurerm_resource_group.tf_test.name
+    ip_address_type="public"
+    dns_name_label="temujinyoungwa"
+    os_type="Linux"
+    container{
+        name="weatherapi"
+        image="temujinyoung/weatherapi"
+        cpu="1"
+        memory="1"
+        ports{
+            port=80
+            protocol="TCP"
+        }
+    }
+}
