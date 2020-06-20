900f768e468568a020c38edb99eb7cd39a81a46f
Nakaw Young <nakawyoung@gmail.com>
2020-06-20 18:03:22 -0400

    Updated pipeline file

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "azurerm"{
     version="2.5.0"
     features{}
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name="tf_rg_blobstore"
+    storage_account_name="tfstoragetemujinyoung"
+    container_name="tfstate"
+    key="terraform.tfstate"
+  }
+}
 resource "azurerm_resource_group" "tf_test"{
     name="tfmainrg"
     location="centralus"
