0296d6d4565f1fa112162f1bd8aec6e6b5bebbf8
Nakaw Young <nakawyoung@gmail.com>
2020-06-21 18:24:23 -0400

    Fixed our pipeline

--- main.tf
+++ main.tf
@@ -22,19 +22,17 @@ resource "azurerm_container_group" "tfcg_test" {
   name                      = "weatherapi"
   location                  = azurerm_resource_group.tf_test.location
   resource_group_name       = azurerm_resource_group.tf_test.name
-
   ip_address_type     = "public"
   dns_name_label      = "temujinyoungwa"
   os_type             = "Linux"
-
   container {
       name            = "weatherapi"
-      image           = "temujinyoung/weatherapi:${var.imagebuild}"
+      image           = "temujinyoung/weatherapi/:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
-
         ports {
             port        = 80
             protocol    = "TCP"
         }
   }
+}
