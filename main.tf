4bdecd73391198288efd505dd9a9611faab67fb6
Nakaw Young <nakawyoung@gmail.com>
2020-06-21 16:54:27 -0400

    Fixed our pipeline

--- main.tf
+++ main.tf
@@ -3,36 +3,38 @@ provider "azurerm"{
     features{}
 }
 terraform {
-  backend "azurerm" {
-    resource_group_name="tf_rg_blobstore"
-    storage_account_name="tfstoragetemujinyoung"
-    container_name="tfstate"
-    key="terraform.tfstate"
-  }
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstoragetemujinyoung"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
 variable "imagebuild" {
   type        = string
   description = "Latest Image Build"
 }
-resource "azurerm_resource_group" "tf_test"{
-    name="tfmainrg"
-    location="Central US"
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "Central US"
 }
-resource "azurerm_container_group" "tfcg_test"{
-    name="weatherapi"
-    location                  = azurerm_resource_group.tf_test.location
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
   resource_group_name       = azurerm_resource_group.tf_test.name
-    ip_address_type="public"
-    dns_name_label="temujinyoungwa"
-    os_type="Linux"
-    container{
-        name="weatherapi"
-        image="temujinyoung/weatherapi:${var.imagebuild}"
-        cpu="1"
-        memory="1"
-        ports{
-            port=80
-            protocol="TCP"
+
+  ip_address_type     = "public"
+  dns_name_label      = "temujinyoungwa"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "temujinyoung/weatherapi:${var.imagebuild}"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
         }
-    }
-}
+  }
