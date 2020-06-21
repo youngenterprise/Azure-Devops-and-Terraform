86a6b7267b3eb5aa1134639d8b246a4b6368ee2f
Nakaw Young <nakawyoung@gmail.com>
2020-06-21 16:46:21 -0400

    Fixed our pipeline

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ resource "azurerm_resource_group" "tf_test"{
 }
 resource "azurerm_container_group" "tfcg_test"{
     name="weatherapi"
-    location=azurerm_resource_group.tf_test.location
-    resource_group_name=azurerm_resource_group.tf_test.name
+    location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
     ip_address_type="public"
     dns_name_label="temujinyoungwa"
     os_type="Linux"
